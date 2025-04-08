a02780cfc0407a8f5f0e0b5a24c982bf8de6799c
Rafael <rafael.julio.dev@outlook.com>
2025-04-08 11:15:16 -0300

    Feat - Init Sensor Data

--- src/infrastructure/repositories/SensorDataRepository.ts
+++ src/infrastructure/repositories/SensorDataRepository.ts
@@ -1,1 +1,17 @@
 // TODO Implement
+
+import { PrismaClient } from "@prisma/client";
+import { ISensorDataRepository } from "../../domain/repositories/ISensorDataRepository";
+import { SensorData } from "../../domain/entities/SensorData";
+import { Result } from "../../shared/core/Result";
+
+
+export class SensorDataRepository implements ISensorDataRepository {
+  constructor(private prisma: PrismaClient) {}
+
+  async create(sensorData: SensorData): Promise<Result<SensorData>> {
+    throw new Error("Method not implemented");
+  }
+
+}
+
